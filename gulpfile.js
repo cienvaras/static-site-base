3bebfc0d7f7b7aba451ac2c4ea2fe168091d7ce0
Andrés Díaz Soto <andres@estudiomanati.com>
2017-09-29 11:11:51 -0600

    Add kss-node styleguide and sourcemaps

--- gulpfile.js
+++ gulpfile.js
@@ -8,9 +8,11 @@
 
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var sourcemaps = require('gulp-sourcemaps');
 var prefix = require('gulp-autoprefixer');
 var connect = require('gulp-connect');
 var eyeglass = require("eyeglass");
+var kss = require('kss');
 
 var sassOptions = {
   outputStyle: 'expanded',
@@ -19,16 +21,9 @@ var sassOptions = {
   }
 };
 
-//
-// Path settings.
-//
-
-var basePath = './';
-
-var paths = {
-  sass: basePath + 'sass',
-  css: basePath + 'css',
-};
+var sassPattern = 'src/sass/**/*.scss';
+var sgPattern = [sassPattern, 'src/sass/**/*.html'];
+var htmlPattern = ['src/**/*.html', '!src/styleguide/**/*.html'];
 
 //
 // Begin Gulp Tasks.
@@ -37,12 +32,13 @@ var paths = {
 //
 // SASS Task
 //
-gulp.task('styles', function () {
-  'use strict';
-  return gulp.src(paths.sass + '/**/*.scss')
+gulp.task('styles:dev', function () {
+  return gulp.src(sassPattern)
+    .pipe(sourcemaps.init())
     .pipe(sass(eyeglass(sassOptions)).on('error', sass.logError))
     .pipe(prefix(["last 2 versions"]))
-    .pipe(gulp.dest(paths.css))
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('src/css'))
     .pipe(connect.reload());
 });
 
@@ -50,16 +46,17 @@ gulp.task('styles', function () {
 // HTML task
 //
 gulp.task('html', function () {
-  gulp.src(basePath + '/**/*.html')
+  return gulp.src(htmlPattern)
     .pipe(connect.reload());
 });
 
 //
-// Watch dev
+// Watch task
 //
 gulp.task('watch', function () {
-  gulp.watch(paths.sass + '/**/*.scss', ['styles']);
-  gulp.watch(basePath + '/**/*.html', ['html']);
+  gulp.watch(sassPattern, ['styles:dev']);
+  gulp.watch(htmlPattern, ['html']);
+  gulp.watch(sgPattern, ['styleguide']);
 });
 
 //
@@ -67,11 +64,25 @@ gulp.task('watch', function () {
 //
 gulp.task('connect', function() {
   connect.server({
-    livereload: true
+    livereload: true,
+    root: 'src'
+  });
+});
+
+//
+// KSS Styleguide task
+//
+
+gulp.task('styleguide', function() {
+  return kss({
+    source: 'src/sass',
+    destination: 'src/styleguide',
+    css: '../css/styles.css',
+    homepage: 'styleguide.md'
   });
 });
 
 //
 // Task declarations.
 //
-gulp.task('dev', ['styles', 'connect', 'watch']);
+gulp.task('dev', ['styles:dev', 'styleguide', 'connect', 'watch']);
