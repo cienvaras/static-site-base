17aa490395e5115edfd825b21731c41c1da3d4d8
Andrés Díaz Soto <andres@estudiomanati.com>
2017-10-27 00:12:03 -0600

    Reorganize gulpfile and add eslint and babel

--- gulpfile.js
+++ gulpfile.js
@@ -4,85 +4,110 @@
  */
 /* eslint-env node */
 
-'use strict';
+'use strict'
 
-var gulp = require('gulp');
-var sass = require('gulp-sass');
-var sourcemaps = require('gulp-sourcemaps');
-var prefix = require('gulp-autoprefixer');
-var connect = require('gulp-connect');
-var eyeglass = require("eyeglass");
-var kss = require('kss');
+var gulp = require('gulp')
+var sass = require('gulp-sass')
+var sourcemaps = require('gulp-sourcemaps')
+var prefix = require('gulp-autoprefixer')
+var connect = require('gulp-connect')
+var eyeglass = require("eyeglass")
+var kss = require('kss')
+var eslint = require('gulp-eslint')
+var babel = require('gulp-babel')
+var concat = require('gulp-concat')
 
 var sassOptions = {
   outputStyle: 'expanded',
   eyeglass: {
     enableImportOnce: false
   }
-};
-
-var sassPattern = 'src/sass/**/*.scss';
-var sgPattern = [sassPattern, 'src/sass/**/*.html'];
-var htmlPattern = ['src/**/*.html', '!src/styleguide/**/*.html'];
+}
 
 //
 // Begin Gulp Tasks.
 //
 
 //
-// SASS Task
+// HTML Dev Workflow.
+//
+gulp.task('html:dev', function () {
+  return gulp.src(['src/**/*html', '!src/sass/*'])
+    .pipe(gulp.dest('.tmp'))
+    .pipe(connect.reload())
+})
+
+//
+// CSS Dev Workflow.
 //
 gulp.task('styles:dev', function () {
-  return gulp.src(sassPattern)
+  return gulp.src('src/sass/**/*.scss')
     .pipe(sourcemaps.init())
     .pipe(sass(eyeglass(sassOptions)).on('error', sass.logError))
     .pipe(prefix(["last 2 versions"]))
     .pipe(sourcemaps.write())
-    .pipe(gulp.dest('src/css'))
-    .pipe(connect.reload());
+    .pipe(gulp.dest('.tmp/css'))
+    .pipe(connect.reload())
+})
+
+//
+// Javascript Dev Workflow.
+//
+gulp.task('js:dev', function () {
+  return gulp.src('src/js/**/*.js')
+    .pipe(sourcemaps.init())
+    .pipe(babel({
+      presets: ['env']
+    }))
+    .pipe(concat('script.js'))
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('.tmp/js'))
 });
 
 //
-// HTML task
+// Javascript linting.
 //
-gulp.task('html', function () {
-  return gulp.src(htmlPattern)
-    .pipe(connect.reload());
-});
+gulp.task('lint', function () {
+  return gulp.src('src/js/**/*.js')
+    .pipe(eslint())
+    .pipe(eslint.format())
+    .pipe(eslint.failAfterError())
+})
 
 //
-// Watch task
-//
-gulp.task('watch', function () {
-  gulp.watch(sassPattern, ['styles:dev']);
-  gulp.watch(htmlPattern, ['html']);
-  gulp.watch(sgPattern, ['styleguide']);
-});
-
-//
-// Gulp Connect dev server
-//
-gulp.task('connect', function() {
-  connect.server({
-    livereload: true,
-    root: 'src'
-  });
-});
-
-//
-// KSS Styleguide task
+// KSS Styleguide.
 //
 
-gulp.task('styleguide', function() {
+gulp.task('styleguide', function () {
   return kss({
     source: 'src/sass',
-    destination: 'src/styleguide',
+    destination: '.tmp/styleguide',
     css: '../css/styles.css',
     homepage: 'styleguide.md'
-  });
-});
+  })
+})
+
+//
+// Dev server.
+//
+gulp.task('connect', function () {
+  connect.server({
+    livereload: true,
+    root: '.tmp'
+  })
+})
+
+//
+// Watch task.
+//
+gulp.task('watch', function () {
+  gulp.watch('src/sass/**/*.scss', ['styles:dev'])
+  gulp.watch('src/**/*.html', ['html:dev'])
+  gulp.watch('src/js/**/*.js', ['lint', 'js:dev'])
+  gulp.watch('src/sass/**/*', ['styleguide'])
+})
 
 //
 // Task declarations.
 //
-gulp.task('dev', ['styles:dev', 'styleguide', 'connect', 'watch']);
+gulp.task('dev', ['html:dev', 'styles:dev', 'js:dev', 'styleguide', 'connect', 'watch'])
